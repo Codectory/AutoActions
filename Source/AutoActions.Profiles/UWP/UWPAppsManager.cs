c7c1bc450758e85d2fc34356b4490ac801684c53
Codectory <heiko.thome@outlook.de>
2025-03-06 14:47:02 +0100

    main merge

---
+++ Source/AutoActions.Profiles/UWP/UWPAppsManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Security.Permissions;
+using System.Security.Principal;
+
+using Windows.Management.Deployment;
+using Windows.ApplicationModel;
+
+namespace AutoActions.UWP
+{
+    public static class UWPAppsManager
+    {
+        static PackageManager manager = new PackageManager();
+
+
+        private const string xboxPassAppFN = "Microsoft.GamingApp_8wekyb3d8bbwe";
+
+
+        public static UWPApp GetUWPApp(string packageNameOrFamilyPackageName, string identity = "")
+        {
+            var package = manager.FindPackageForUser(WindowsIdentity.GetCurrent().User.Value, packageNameOrFamilyPackageName);
+            if (package == null)
+                return GetUWPAppCompatible(packageNameOrFamilyPackageName, identity);
+            return new UWPApp(package);
+        }
+
+        private static UWPApp GetUWPAppCompatible(string familyPackageName, string identity)
+        {
+            foreach (var package in manager.FindPackagesForUser(WindowsIdentity.GetCurrent().User.Value))
+            {
+                try
+                {
+                    UWPApp uwpApp = new UWPApp(package);
+                    if (uwpApp.FamilyPackageName.Equals(familyPackageName) && (string.IsNullOrEmpty(identity) || uwpApp.Identity.Equals(identity)))
+                        return uwpApp;
+                }
+                catch {}
+            }
+            return null;
+        }
+
+
+        public static List<ApplicationItem> GetUWPApps()
+        {
+
+            Globals.Logs.Add($"Retrieving UWP apps...", false);
+
+            List<ApplicationItem> uwpApps = new List<ApplicationItem>();
+            IEnumerable<Package> packages = manager.FindPackagesForUser(WindowsIdentity.GetCurrent().User.Value);
+            try
+            {
+                foreach (var package in packages)
+                {
+                    string s = package.DisplayName;
+                    if (package.IsFramework || package.IsResourcePackage || package.SignatureKind != PackageSignatureKind.Store )
+                    {
+                        continue;
+                    }
+
+                    try
+                    {
+                        if (package.InstalledLocation == null)
+                        {
+                            continue;
+                        }
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                    try
+                    {
+                        UWPApp uwpApp = new UWPApp(package);
+                        if (!string.IsNullOrEmpty(uwpApp.ApplicationID))
+                         uwpApps.Add(new ApplicationItem(uwpApp));
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+                return uwpApps.OrderBy(u => u.DisplayName).ToList();
+            }
+            catch (Exception ex)
+            {
+                Globals.Logs.AddException($"Retrieving UWP apps failed.", ex);
+                throw;
+            }
+        }
+
+        public static void StartUWPApp(string FamilyPackage, string applicationID)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "explorer.exe";
+            process.StartInfo.Arguments = $"shell:AppsFolder\\{FamilyPackage}!{applicationID}";
+            process.Start();
+
+        }
+
+    }
+}
--- Source/AutoActions.ProjectResources/Properties/AssemblyInfo.cs
+++ Source/AutoActions.ProjectResources/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// Allgemeine Informationen über eine Assembly werden über die folgenden
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("AutoActions.ProjectResources")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AutoActions.ProjectResources")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("bc368558-8b03-4310-b4d3-efe3ba7bccb6")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
-// indem Sie "*" wie unten gezeigt eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
