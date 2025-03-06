8af77ba6b2a9bae16aa43b396f8be06de7d5d3d4
Codectory <heiko.thome@outlook.de>
2025-03-06 14:40:46 +0100

    manifest update

--- Source/AutoActions/Properties/AssemblyInfo.cs
+++ Source/AutoActions/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Codectory")]
 [assembly: AssemblyProduct("AutoActions")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
